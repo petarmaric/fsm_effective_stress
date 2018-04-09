793e542d54c7175cb585dbb9ab633630e577991a
Petar Marić <petar.maric@gmail.com>
2018-04-09 08:15:31 +0200

    Initial release, decoupled the code from https://bitbucket.org/petar/fsm_damage_analysis for reusability

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='fsm_effective_stress',
+    version='1.0.0',
+    url='https://bitbucket.org/petar/fsm_effective_stress',
+    license='BSD',
+    author='Petar Maric',
+    author_email='petarmaric@uns.ac.rs',
+    description='Python library that uses the rheological-dynamical analogy '\
+                '(RDA) to compute damage and effective buckling stress in '\
+                'prismatic shell structures.',
+    long_description=open('README').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
+    platforms='any',
+    py_modules=['fsm_effective_stress'],
+)
