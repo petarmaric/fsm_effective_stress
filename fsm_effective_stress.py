21ddcd3929c2f3a4c103ee553e6f74cf950c7ea7
Petar Mari? <petar.maric@gmail.com>
2018-04-09 08:15:31 +0200

    Initial release, decoupled the code from https://bitbucket.org/petar/fsm_damage_analysis for reusability

---
+++ fsm_effective_stress.py
@@ -0,0 +1,8 @@
+__version__ = '1.0.0'
+
+
+def compute_damage(omega, omega_d):
+    return 1 - (omega_d/omega)**2
+
+def compute_effective_stress(omega, omega_d, sigma_d):
+    return sigma_d * (omega/omega_d)**2
